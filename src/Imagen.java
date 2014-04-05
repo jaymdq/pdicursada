98188cc7ded283aaf922e3218edb671a6b5b7034
Hernan <hernangrocha@yahoo.com.ar>
2014-04-05 17:36:55 -0300

    Metodos de Filtro

--- src/Imagen.java
+++ src/Imagen.java
@@ -1,24 +1,63 @@
-import java.awt.Color;
-
 import ij.ImagePlus;
 import ij.gui.NewImage;
 
 
 public class Imagen {
 
-	public static void setPixel (ImagePlus im, int x, int y, Color c){
-		im.getProcessor().setColor(c);
+	// Setear la intensidad de un pixel (x,y)
+	public static void setPixel (ImagePlus im, int x, int y, int color){
+		
+		im.getProcessor().setColor(color);
 		im.getProcessor().drawDot(x, y);
+		
 	}
 	
-	public static Color getPixel (ImagePlus im, int x, int y){
-		// TODO Implementar esto (Brian)
-		return new Color(0);
+	// Obtener la intensidad de un pixel (x,y)
+	public static int getPixel (ImagePlus im, int x, int y){
+		
+		return (int) im.getProcessor().getPixelValue(x, y);
+		
 	}
 	
-	public ImagePlus aplicarFiltro(ImagePlus im, int[][] matriz){
-		//NewImage.createImage("nueva", width, height, nSlices, bitDepth, options)
-		return null;
+	// Aplicar un filtro a una imagen completa
+	public ImagePlus aplicarFiltro(ImagePlus im, MatrizConvolucion matriz){
+		
+		int ancho = im.getWidth();
+		int alto = im.getHeight();
+		int radio = matriz.getRadio();
+		
+		// Crear imagen de resultado
+		ImagePlus nueva = NewImage.createImage("nueva", ancho, alto, 1, 8, 0);
+		
+		// Recorrer imagen (excepto bordes)
+		for (int i = radio; i <= alto - radio; i++){
+			for (int j = radio; j <= ancho - radio; j++){
+				int color = aplicarFiltro(im, matriz, i, j);
+				setPixel(nueva, i, j, color);
+			}
+		}
+		
+		// Devolver imagen nueva
+		return nueva;
+	}
+	
+	// Aplicar filtro a un pixel (i,j)
+	public static int aplicarFiltro (ImagePlus im, MatrizConvolucion matriz, int i, int j){
+		
+		int tam = matriz.getTam();
+		int radio = matriz.getRadio();
+		int acumulador = 0;
+		int normalizacion = 9;
+		
+		// Recorrer matriz de convolucion
+		for (int x = 0; x < tam; x++){
+			for (int y = 0; y < tam; y++){
+				acumulador += matriz.get(x, y) * getPixel(im, i - radio + x, j - radio + y);
+			}
+		}
+		
+		// Devolver valor normalizado
+		return acumulador / normalizacion;
 	}
 	
 }
---
+++ src/MatrizConvolucion.java
@@ -0,0 +1,26 @@
+
+public class MatrizConvolucion {
+
+	private int[][] matriz;
+	private int tam;
+	private int radio;
+	
+	public MatrizConvolucion (int tam, int[][] matriz){
+		this.matriz = matriz;
+		this.tam = tam;
+		this.radio = tam / 2;
+	}
+	
+	public int getRadio(){
+		return radio;
+	}
+	
+	public int getTam(){
+		return tam;
+	}
+	
+	public int get(int i, int j){
+		return matriz[i][j];
+	}
+	
+}
